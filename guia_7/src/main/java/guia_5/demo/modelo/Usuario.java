da5adea01520352fb1670281b8c0f2e912825e7b
LuisRicardo77 <luissayas2018@gmail.com>
2024-04-16 22:34:56 -0500

    first commit

---
+++ guia_7/src/main/java/guia_5/demo/modelo/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia_5.demo.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ldric
+ */
+@Data
+public class Usuario {
+    private String cedula;
+    private String nombre;
+    private String email;
+    private String clave;
+}
